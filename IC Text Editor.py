69f91deec781df038c4e37a18e967fd96142074c
IgorCielniak <138619689+IgorCielniak@users.noreply.github.com>
2023-08-04 16:23:18 +0200

    Update IC Text Editor.py
    version 8.2

--- IC Text Editor.py
+++ IC Text Editor.py
@@ -47,7 +47,7 @@ class TextEditor:
     def Chart(self):
         self.Charte()
 
-    def opinion_comments(self):
+    def contact(self):
         webbrowser.open("https://icprogramers-com.webnode.page/kontakt/")
 
     def add_tab_with_table(self):
@@ -72,16 +72,16 @@ class TextEditor:
         messagebox.showinfo("Table", "Table should be in new tab")
 
     def info(self):
-        version = 7.8
+        version = 8.2
         messagebox.showinfo("Info", f"""
         version {version} 
         IC Text Editor was maked by Igor Cielniak.
-        © 2023 IC programers all rights reserved.""")
+        © 2023 Igor Cielniak all rights reserved.""")
 
 
     def licencja(self):
         messagebox.showinfo("Licence", """
-Copyright IC programers 2023
+Copyright 2023 Igor Cielniak
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -97,48 +97,6 @@ limitations under the License.
 
         """)
     
-    def prywatność(self):
-        messagebox.showinfo("Privacy", """
-PRIVACY STATEMENT
-1.
-The IC Programers company with its registered office in Marki, ul.Piotrówka 14, hereinafter referred to as the "Administrator", cares for the protection of the privacy of its Clients and Users, therefore this Privacy Statement sets out the rules for the processing of personal data that are collected by the Administrator as part of the provision services.
-1.
-Scope of personal data processing:
-The Administrator processes personal data that has been provided voluntarily by Clients or Users when using the services provided by IC Programers. These data may include, among others: name, surname, e-mail address, telephone number, identification data, information about preferences and interests.
-2.
-Purpose of personal data processing:
-Personal data is processed by the Administrator in order to:
-providing services in accordance with contracts concluded with customers,
-ensuring the safety of using the services provided by IC Programers,
-adapting the offer and information provided to Customers and Users to their preferences and interests,
-to pursue the Administrator's legitimate interests, such as investigating or defending against claims, auditing, statistics.
-3.
-Legal basis for the processing of personal data:
-The legal basis for the processing of personal data is:
-an agreement concluded with the Customer in order to provide services,
-the legitimate interests of the Administrator,
-consent expressed by the Customer or User to the processing of personal data for a specific purpose.
-4.
-Recipients of personal data:
-Personal data of Clients and Users may be transferred to entities cooperating with IC Programers, such as technical service providers, consultants, auditors, as part of the implementation of services provided by the Administrator.
-5.
-Personal data storage period:
-Personal data will be stored by the Administrator for the period necessary to achieve the purposes for which they were collected, and for the period required by law.
-6.
-Security of personal data:
-The administrator provides appropriate technical and organizational measures to protect personal data against unauthorized access, loss, damage or destruction. These activities include e.g. securing IT infrastructure, monitoring, training employees in the field of privacy protection.
-7.
-Consent to the processing of personal data:
-The processing of personal data is carried out on the basis of the consent expressed by the Client/User or on the basis of another legal basis referred to in point 3 of this Privacy Statement. Consent to the processing of personal data may be withdrawn at any time by the Customer/User, which will not affect the lawfulness of the processing carried out before the withdrawal of consent.
-8.
-Final Provisions:
-This Privacy Statement is an integral part of the agreement concluded between the Client/User and IC Programers regarding the provision of services. The Administrator reserves the right to change this Privacy Statement at any time, which will be communicated to Clients/Users in an appropriate manner. In the event of a change in the provisions of this Privacy Statement, the Customer/User will be able to withdraw consent to the processing of personal data in accordance with the new provisions.
-9.
-By using the software, you agree to the processing of personal data in accordance with the above subsections.
-
-                            """)
-
-
 
     def find_text(self):
         text_widget = self.text_areas[self.current_tab]
@@ -238,9 +196,8 @@ By using the software, you agree to the processing of personal data in accordanc
         about_menu = tk.Menu(menu, tearoff=0)
         about_menu.add_command(label="Info", command=self.info)
         about_menu.add_command(label="Licence", command=self.licencja)
-        about_menu.add_command(label="Privacy", command=self.prywatność)
         about_menu.add_command(label="Website", command=self.strona)
-        about_menu.add_command(label="opinion/comments", command=self.opinion_comments)
+        about_menu.add_command(label="contact", command=self.contact)
         menu.add_cascade(label="About", menu=about_menu)
 
     def change_font_size(self):
@@ -256,7 +213,7 @@ By using the software, you agree to the processing of personal data in accordanc
         quit
 
     def strona(self):
-        webbrowser.open("https://icprogramers-com.webnode.page")
+        webbrowser.open("https://igorcielniak-com.webnode.page")
 
     def add_addon_password(self):
         try:
